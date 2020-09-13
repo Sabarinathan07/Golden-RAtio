6bbf99745f6e6b09b67291d6d727d2ec24cd05bb
Sabarinathan07 <64265337+Sabarinathan07@users.noreply.github.com>
2020-09-13 22:20:50 +0530

    final

--- app/src/main/java/com/sabari/bottomnav/SharePrefManager.java
+++ app/src/main/java/com/sabari/bottomnav/SharePrefManager.java
@@ -54,52 +54,6 @@ import android.content.SharedPreferences;
         return true;
     }
 
-    public boolean quizid(int quizid){
-
-        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        editor.putInt(KEY_QUIZ_ID, quizid);
-
-        editor.apply();
-
-        return true;
-    }
-
-
-    public boolean isLoggedIn(){
-        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
-        if(sharedPreferences.getString(KEY_USERNAME, null) != null){
-            return true;
-        }
-        return false;
-    }
-
-    public boolean logout(){
-        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.clear();
-        editor.apply();
-        return true;
-    }
-
-
-    public static int getId(Context mCtx){
-        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
-        return sharedPreferences.getInt(KEY_USER_ID, 0);
-    }
-
-    public static int getQuizId(Context mCtx){
-        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
-        return sharedPreferences.getInt(KEY_QUIZ_ID, 0);
-    }
-
-
-
-    public String getUserEmail(){
-        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
-        return sharedPreferences.getString(KEY_USER_EMAIL, null);
-    }
 
     public String getName(){
         SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
---
+++ php/includes/Constants.php
@@ -0,0 +1,6 @@
+<?php 
+	define('DB_NAME','id14485003_test');
+	define('DB_USER','id14485003_root');
+	define('DB_PASSWORD','zU91jTjkpii>Vs(A');
+	define('DB_HOST','localhost');
+	?>
